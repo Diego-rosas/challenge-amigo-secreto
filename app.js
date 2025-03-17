d0e2357d435fa1d14f6f42bee0f93c25a1a6ad0d
Diego Rosas <diegorosas19@gmail.com>
2025-03-17 00:14:19 -0300

    Inicio da parte logica

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigosParaSeremSorteados = [];
+
+function exibirLista(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function adicionarAmigo() {
+    let addcionar = document.querySelector('input').value;
+    if (addcionar == '') {
+        alert('Digite um nome!');
+    } else {
+        amigosParaSeremSorteados.push(addcionar);
+        exibirLista('ul', amigosParaSeremSorteados);
+        console.log(amigosParaSeremSorteados);
+        limparCampo();
+    }
+}
